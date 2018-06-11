8848cea549fa86204b5dc60ab9e8a9a8e55eafbf
Vasiliy <vkraskov@gmail.com>
2018-06-11 15:11:44 +0300

    dqnsnake.mirror.v10d

--- agent.py
+++ agent.py
@@ -9,6 +9,7 @@ from keras.optimizers import Adam
 from keras.layers import Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D, Permute
 from keras.initializers import RandomUniform
+import sys
 
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
@@ -21,6 +22,20 @@ STATE_DXY = 17
 STATE_SIZE = STATE_DXY*STATE_DXY
 STATE_CLIP_DXY = 5
 STATE_NEAR_DXY = 3
+
+ACT_FORWARD = 0
+ACT_BACK = 1
+ACT_LEFT = 2
+ACT_RIGHT = 3
+
+ELEM_BLANK = 0.0
+ELEM_BORDER = -1.0
+ELEM_SNAKE = -0.99
+ELEM_SNAKE_HEAD = -0.98
+ELEM_SNAKE_TAIL = -0.97
+ELEM_FOOD = 1.0
+
+elem2chr = { ELEM_BLANK: " ", ELEM_BORDER: "#", ELEM_SNAKE: "X", ELEM_SNAKE_HEAD: "8", ELEM_SNAKE_TAIL: "O", ELEM_FOOD: "*" }
 
 ############
 
@@ -64,23 +79,23 @@ class Agent:
 		in3 = Input(shape=(1,))
 		dense_3_1 = Dense(256, activation='relu')(in3)
 
-		in4 = Input(shape=(STATE_NEAR_DXY, STATE_NEAR_DXY, 1))
-		conv2d_4_1 = Conv2D(16, (3, 3), activation = 'relu')(in4)
-		conv2d_4_2 = Conv2D(32, (1, 1), activation = 'relu')(conv2d_4_1)
-		flatten_4 = Flatten()(conv2d_4_2)
-		dense_4_1 = Dense(256, activation='relu')(flatten_4)
-
-		#joined = keras.layers.Merge()([dense_1_1, dense_2_1, dense_3_1])
-		joined = keras.layers.Merge()([dense_1_1, dense_2_1, dense_3_1, dense_4_1])
+		joined = keras.layers.Merge()([dense_1_1, dense_2_1, dense_3_1])
 		dense_f_1 = Dense(256, activation='relu')(joined)
 		dense_f_2 = Dense(self.action_size, activation='linear')(dense_f_1)
 
-		#model = Model(inputs = [in1 , in2, in3], outputs = dense_f_2)
-		model = Model(inputs = [in1 , in2, in4, in3], outputs = dense_f_2)
+		model = Model(inputs = [in1 , in2, in3], outputs = dense_f_2)
                 model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate))
 		model.summary()
 
         	return model
+
+	def render_dxy_arr(self, arr, beg_pos, end_pos):
+		for y in range(beg_pos[1], end_pos[1]):
+			for x in range(beg_pos[0],end_pos[0]):
+				elem = arr[x][y]
+				echr = elem2chr[elem]
+				sys.stdout.write(echr)
+			sys.stdout.write('\n')
 
 	def remember(self, state, action, reward, next_state, done, step, score):
 		nn_step = (2.*float(score)/self.maxsteps-1)
@@ -109,30 +124,22 @@ class Agent:
 		# clipped view
 		state_clip = self.get_state_clip(state, STATE_CLIP_DXY)
 		np_state_clip =  np.asarray(state_clip).reshape(1, STATE_CLIP_DXY, STATE_CLIP_DXY, 1)
-		# near view
-		state_near = self.get_state_clip(state, STATE_NEAR_DXY)
-		np_state_near =  np.asarray(state_near).reshape(1, STATE_NEAR_DXY, STATE_NEAR_DXY, 1)
 		# score as tails grows
 		nn_step = (2.*float(score)/self.maxsteps-1)
 		np_step_arr =  np.asarray(nn_step).reshape(1, 1)
 		# predict
-		#self.act_values = self.model.predict([np_state, np_state_clip, np_step_arr])
-		self.act_values = self.model.predict([np_state, np_state_clip, np_state_near, np_step_arr])
+		self.act_values = self.model.predict([np_state, np_state_clip, np_step_arr])
 		#if np.argmax(act_values[0]) == ACT_BACK: 
 		#	print act_values, np.argmax(act_values[0]), action2str[np.argmax(act_values[0])]
 		return np.argmax(self.act_values[0])  # returns action
 
 	def train_batch(self, X_batch, X_extra, y_batch):
 		X_batch_clip = self.get_state_clip_batch(X_batch, STATE_CLIP_DXY)
-		X_batch_near = self.get_state_clip_batch(X_batch, STATE_NEAR_DXY)
-		#return self.model.fit([X_batch, X_batch_clip, X_extra], y_batch, epochs=1, verbose=0)
-		return self.model.fit([X_batch, X_batch_clip, X_batch_near, X_extra], y_batch, epochs=1, verbose=0)
+		return self.model.fit([X_batch, X_batch_clip, X_extra], y_batch, epochs=1, verbose=0)
 
 	def predict_batch(self, X_batch, X_extra):
 		X_batch_clip = self.get_state_clip_batch(X_batch, STATE_CLIP_DXY)
-		X_batch_near = self.get_state_clip_batch(X_batch, STATE_NEAR_DXY)
-		#return self.model.predict_on_batch([X_batch, X_batch_clip, X_extra])
-		return self.model.predict_on_batch([X_batch, X_batch_clip, X_batch_near, X_extra])
+		return self.model.predict_on_batch([X_batch, X_batch_clip, X_extra])
 
 	def get_state_byid(self, mem_id):
 		return self.sdict[mem_id]
@@ -156,6 +163,7 @@ class Agent:
 		X_extra = np.vstack(w1)
 		X_extra = np.asarray(X_extra).reshape(batch_size, 1)
 		y_batch = self.predict_batch(X_batch, X_extra)
+		y_batch_save = y_batch
 
 		X_batch_s2 = np.vstack(s2)
 		X_batch_s2 = np.asarray(X_batch_s2).reshape(batch_size, STATE_DXY, STATE_DXY, 1) 
@@ -197,7 +205,50 @@ class Agent:
 			self.gamma*self.gamma*r2[2]*(1-d1)*(1-d2[1])*(1-d2[2]) + \
 			self.gamma*self.gamma*self.gamma*np.max(self.predict_batch(X_batch_s3, X_extra_s3), 1)*(1-d1)*(1-d2[1])*(1-d2[2])
 
-		return X_batch, X_extra, y_batch
+		X_batch_flip = np.vstack(s1)
+		X_batch_flip = np.asarray(X_batch_flip).reshape(batch_size, STATE_DXY, STATE_DXY)
+		a1_flip = a1
+		#print "X_batch_flip", X_batch_flip[0]
+		#s_flip_arr = X_batch_flip[0]
+		#s_flip_arr = s_flip_arr[:,::-1]
+		#print "s_flip_arr", s_flip_arr
+		for k in range(batch_size):
+			s_flip_arr = X_batch_flip[k]
+			#self.render_dxy_arr(s_flip_arr, [0, 0], [STATE_DXY-1, STATE_DXY-1])
+			s_flip_arr = s_flip_arr[::-1,:,]
+			#print "==="
+			#self.render_dxy_arr(s_flip_arr, [0, 0], [STATE_DXY-1, STATE_DXY-1])
+			X_batch_flip[k] = s_flip_arr
+			
+			if a1_flip[k] == ACT_LEFT:
+				a1_flip[k] = ACT_RIGHT
+			elif a1_flip[k] == ACT_RIGHT:
+				a1_flip[k] = ACT_LEFT
+		X_batch_flip = np.asarray(X_batch_flip).reshape(batch_size, STATE_DXY, STATE_DXY, 1) 
+		y_batch_flip = self.predict_batch(X_batch_flip, X_extra)
+		#y_batch_flip = y_batch_save
+
+		X_batch_s3_flip = X_batch_s3
+		X_batch_s3_flip = np.asarray(X_batch_s3_flip).reshape(batch_size, STATE_DXY, STATE_DXY)
+		for k in range(batch_size):
+			s_flip_arr = X_batch_s3_flip[k]
+			#self.render_dxy_arr(s_flip_arr, [0, 0], [STATE_DXY, STATE_DXY])
+			#print "==="
+			s_flip_arr = s_flip_arr[::-1,:,]
+			#self.render_dxy_arr(s_flip_arr, [0, 0], [STATE_DXY, STATE_DXY])
+			X_batch_s3_flip[k] = s_flip_arr
+		X_batch_s3_flip = np.asarray(X_batch_s3_flip).reshape(batch_size, STATE_DXY, STATE_DXY, 1) 
+
+		y_batch_flip[np.arange(batch_size), a1_flip] = \
+			r1 + \
+			self.gamma*r2[1]*(1-d1)*(1-d2[1])*(1-d2[2]) + \
+			self.gamma*self.gamma*r2[2]*(1-d1)*(1-d2[1])*(1-d2[2]) + \
+			self.gamma*self.gamma*self.gamma*np.max(self.predict_batch(X_batch_s3_flip, X_extra_s3), 1)*(1-d1)*(1-d2[1])*(1-d2[2])
+
+		return  np.concatenate((X_batch, X_batch_flip), axis=0),\
+			np.concatenate((X_extra, X_extra), axis=0),\
+			np.concatenate((y_batch,y_batch_flip), axis=0)
+		#return X_batch_flip, X_extra, y_batch_flip
 
 
 #		# https://gist.github.com/kkweon/5605f1dfd27eb9c0353de162247a7456
--- t.py
+++ t.py
@@ -1,3 +1,11 @@
 
-for z in range(3):
-	print z
+import numpy as np
+
+a = np.array([[[0, 1, 2], [3, 4, 5], [6, 7, 8]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]])
+print a
+print "=="
+print a[:,:,::-1] 
+
+
+
+
